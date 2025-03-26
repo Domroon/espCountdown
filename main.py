92150b095fcd850c0695e58bbb407379c4c0f5c8
domroon <info@domroon.de>
2025-03-26 20:16:01 +0100

    implement digit class

--- main.py
+++ main.py
@@ -1,5 +1,76 @@
+import machine
+import time
+
+from neopixel import NeoPixel
+
+DIGIT_0_PIN = 14
+
+LINE = [    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ]
+
+class Digit:
+    def __init__(self, neopixel):
+        self.np: NeoPixel = neopixel
+        self.lines = []
+        self.screen = []
+
+    def _generate_lines(self, pixelfield) -> list:
+        for _ in range(16):
+            line = []
+            for _ in range(16):
+                line.append(pixelfield.pop(0))
+            self.lines.append(line)
+
+    def _line_is_odd(self, line_number: int) -> bool:
+        if line_number % 2 == 1:
+            return True
+        else:
+            return False
+    
+    def _reverse_all_odd_lines(self):
+        for line_number, line in enumerate(self.lines):
+            if self._line_is_odd(line_number):
+                line.reverse()
+
+    def _append_all_lines_to_screen(self) -> None:
+        for line in self.lines:
+            for _ in range(16):
+                self.screen.append(line.pop())
+
+    def show_on_screen(self, pixelfield) -> None:
+        self._generate_lines(pixelfield)
+        self._reverse_all_odd_lines()
+        self._append_all_lines_to_screen()
+        self.np.fill([0, 0, 0])
+        for i, pixel in enumerate(self.screen):
+            if pixel:
+                self.np[i] = [10, 10, 10]
+        self.np.write()
+        input()
+        self.np.fill([0, 0, 0])
+        self.np.write()
+
+
 def main():
-    print("hello world")
+    neopixel = NeoPixel(machine.Pin(DIGIT_0_PIN), 256)
+    digit_0 = Digit(neopixel)
+    digit_0.show_on_screen(LINE)
+
 
 if __name__ == '__main__':
     main()
